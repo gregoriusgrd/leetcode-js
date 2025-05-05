c4ff4fc2b9af6c2f5a21c37121ef96c61fc691a9
Gregorius G <gregorius.gerald21@gmail.com>
2025-05-05 22:45:43 +0700

    add

---
+++ easy/reverse-string.js
@@ -0,0 +1,11 @@
+// 344. Reverse String
+// https://leetcode.com/problems/reverse-string/description/
+
+var reverseString = function (s) {
+  return s.reverse();
+};
+
+/*
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+*/
