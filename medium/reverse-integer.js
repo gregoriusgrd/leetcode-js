8bfe940afa8cb17cde7c032f92d7be5eda98eca2
Gregorius G <gregorius.gerald21@gmail.com>
2025-05-06 00:34:12 +0700

    update

--- medium/reverse-integer.js
+++ medium/reverse-integer.js
@@ -3,10 +3,10 @@
 
 var reverse = function (x) {
   const absolute = Math.abs(x);
-  const invert = absolute.toString();
-  const swap = invert.split("").reverse().join("");
-  const invertBack = parseInt(swap);
-  const result = invertBack * Math.sign(x);
+  const convert = absolute.toString();
+  const swap = convert.split("").reverse().join("");
+  const convertBack = parseInt(swap);
+  const result = convertBack * Math.sign(x);
   const limit = Math.pow(2, 31);
   if (result < -limit || result > limit - 1) return 0;
   return result;
